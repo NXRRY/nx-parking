d0dca5908226fb4ce3a8ba3d3e7ae6b213370fb6
JOXO_xD <88522315+NXRRY@users.noreply.github.com>
2026-02-23 14:00:54 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = Config or {}
+
+Config.Debug = false -- เปิดโหมด Debug เพื่อดูข้อมูลที่จับได้ในคอนโซล (ไม่แนะนำให้เปิดในเซิร์ฟเวอร์จริง)
+Config.notifyType = 'chat' -- เลือกประเภทการแจ้งเตือน ('qb',chat ,'okok' หรืออื่นๆ)
---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_fxv2_oal 'yes'
+author 'DADDT'
+description 'A parking system for FiveM servers.'
+version '0.1.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    'client/main.lua'
+}
