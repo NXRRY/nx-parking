c422fb1aa03b27d87b8cc9839e80d47703ac8d32
Benjamin_1x <88522315+NXRRY@users.noreply.github.com>
2026-03-02 13:02:30 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 use_fxv2_oal 'yes'
 author 'NXRRY'
 description 'A parking system for FiveM servers.'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
